843506fbb1360b84fe0c6ec9ca33c56ae1d4a2a4
SunilDokuru <skdokuru@neiu.edu>
2015-11-24 12:56:19 -0600

    Create Test.R, Train.R

---
+++ Test.R, Train.R
@@ -0,0 +1,20 @@
+// Have used small proportions of 'R' to filter my Data. The code is given below.
+
+
+// Test Files
+mydata1 = read.table("/Users/Dokuru/Desktop/Informatics_HW3/ml-100k/u1.test")
+mydata1[4] <- NULL
+write.table(mydata1, "/Users/Dokuru/Desktop/Informatics_HW3/test/test1.txt", sep="\t",row.names=FALSE,col.names=FALSE)
+
+mydata1 = read.table("/Users/Dokuru/Desktop/Informatics_HW3/test/test1.txt")
+itemList1 = duped.data <- unique(mydata1[,2])
+itemList1 = sort(itemList1)
+maxTest1 = tail(itemList1, n = 1)
+write(maxTest1, "/Users/Dokuru/Desktop/Informatics_HW3/test/lengths.txt", append="false")
+
+
+//Train Files
+
+mydata1 = read.table("/Users/Dokuru/Desktop/Informatics HW3/ml-100k/u1.base")
+mydata1[4] <- NULL
+write.table(mydata1, "/Users/Dokuru/Desktop/train/train1.txt", sep=",",row.names=FALSE,col.names=FALSE)
